a3b1983dc9ff1d11b302cdd640790eddbb6db7bd
Wyatt Roehler <wyatt.roehler@naveego.com>
2021-03-22 15:11:05 -0400

    feat(license): added support for copying license file

--- PluginDb2/Program.cs
+++ PluginDb2/Program.cs
@@ -23,6 +23,19 @@ namespace PluginDb2
                 var assemblyPath = Assembly.GetExecutingAssembly().Location;
                 var installDirectory = Path.GetDirectoryName(assemblyPath);
 
+                if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("LD_LICENSE_PATH")))
+                {
+                    try
+                    {
+                        var licenseSourceDirectory = Environment.GetEnvironmentVariable("LD_LICENSE_PATH");
+                        var licenseTargetDirectory = Path.Join(installDirectory, "/clidriver/license");
+                        DirectoryCopy(licenseSourceDirectory, licenseTargetDirectory, true);
+                    }
+                    catch
+                    {
+                    }
+                }
+
                 if (string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("LD_LIBRARY_PATH")) && !RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
                     Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", $"{installDirectory}/clidriver/lib");
@@ -88,6 +101,42 @@ namespace PluginDb2
                 Logger.Error(e, e.Message);
                 Logger.CloseAndFlush();
                 throw;
+            }
+        }
+        
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+        
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);        
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
             }
         }
     }
